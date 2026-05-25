44eccb1a312e93c2570e50ea6bb5466b3bba656a
LgCookie <lgc2333@126.com>
2026-05-25 21:58:17 +0800

    ci: fix cloudflare workers build

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { fileURLToPath } from 'node:url'
 
+import { cloudflare } from '@cloudflare/vite-plugin'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 import VueRouter from 'vue-router/vite'
-
-// import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [
@@ -19,7 +18,7 @@ export default defineConfig({
       dts: 'src/auto-imports.d.ts',
     }),
     Components({ dts: 'src/components.d.ts' }),
-    // cloudflare()
+    cloudflare() as any,
   ],
   resolve: {
     alias: {
