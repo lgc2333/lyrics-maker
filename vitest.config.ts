7d157db780d5400aae8f74b0fb7e90b2db696e6b
LgCookie <lgc2333@126.com>
2026-05-29 00:06:24 +0800

    test: vitest coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,20 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   plugins: [vue()],
   test: {
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html', 'lcov'],
+      reportsDirectory: './coverage',
+      include: ['src/**/*.{ts,vue}'],
+      exclude: [
+        'src/**/*.spec.ts',
+        'src/**/*.d.ts',
+        'src/auto-imports.d.ts',
+        'src/components.d.ts',
+        'src/typed-router.d.ts',
+        'src/test/**',
+      ],
+    },
     environment: 'happy-dom',
     globals: true,
     include: ['src/**/*.spec.ts'],
